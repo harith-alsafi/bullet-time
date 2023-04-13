6a296e98ec703a9ebc99a27e45ce9b5b36d6f233
Harith Al-Safi <harith.alsafi@gmail.com>
2023-04-13 03:11:21 +0300

    setup

---
+++ src/player.py
@@ -0,0 +1,14 @@
+import constants as ct
+from constants import pygame as pg
+
+class Player:
+    rect:pg.Rect = pg.Rect((ct.PLAYER_X_START, ct.PLAYER_Y_START, ct.PLAYER_WIDTH, ct.PLAYER_WIDTH))
+    x:int = ct.PLAYER_X_START
+    y:int = ct.PLAYER_Y_START
+
+    def init(self):
+        return
+    
+    def draw(self, screen:pg.Surface):
+        pg.draw.rect(screen, ct.PLAYER_COLOR, self.rect)
+
